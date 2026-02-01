c79fa5db1f63a0a844faf8519a774b9b654e0d49
Miroslav Kušnír <miroslav.kusnir@mkdigital.sk>
2026-02-01 16:29:44 +0100

    networking-ui-fix (#11)

--- src/presentation/screens/networking/UserCard.tsx
+++ src/presentation/screens/networking/UserCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Card, Text } from 'react-native-paper';
+import { Text } from 'react-native-paper';
 import { User } from '../../../domain/models/User';
 import { space2, space4 } from '../../foundation/dimensions';
+import { AppCard } from '../../components';
 
 interface UserCardProps {
   user: User;
@@ -10,17 +11,15 @@ interface UserCardProps {
 
 export const UserCard = ({ user }: UserCardProps) => {
   return (
-    <Card style={styles.card} mode="outlined">
-      <Card.Content>
-        <Text variant="titleMedium">{user.name}</Text>
-        <Text variant="bodyMedium" style={styles.email}>
-          {user.email}
-        </Text>
-        <Text variant="bodySmall" style={styles.address}>
-          {user.address.street}, {user.address.city}
-        </Text>
-      </Card.Content>
-    </Card>
+    <AppCard>
+      <Text variant="titleMedium">{user.name}</Text>
+      <Text variant="bodyMedium" style={styles.email}>
+        {user.email}
+      </Text>
+      <Text variant="bodySmall" style={styles.address}>
+        {user.address.street}, {user.address.city}
+      </Text>
+    </AppCard>
   );
 };
 
